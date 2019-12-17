74e0e68b5617b10f0d3bc777bca9c709c6cd189b
Dincius <diana.meksriunaite@gmail.com>
2019-12-17 21:35:30 +0200

    ...

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-var galleryLoad = async function() {
+async function galleryLoad() {
   const response = await fetch('https://picsum.photos/v2/list');
   const images = await response.json();
 
   for (var i = 0; i < images.length; i++) {
     
-    let galleryItem = document.createElement('img');
+    let galleryItem = document.createElement('div');
     galleryItem.classList.add('gallery-item');
-    galleryItem.src = "${images[i].download_url}";
+    galleryItem.innerHTML = `<img class="gallery-image" src="${images[i].download_url}" alt="gallery image"/>`;
     document.body.appendChild(galleryItem);
     }
 }
   
-// galleryLoad();
+galleryLoad();
